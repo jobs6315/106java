14f5f3017a7f9087af05735514260c09746ada64
631582636 <631582636@qq.com>
2022-08-16 21:28:10 +0800

    试一下

---
+++ j-2022-08-16/src/test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Lenovo
+ * Date: 2022-08-16
+ * Time: 21:26
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+    }
+}
